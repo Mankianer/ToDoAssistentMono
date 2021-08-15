95109db3559b092bf442126fda6a5d711a6234a6
Mankianer <Marvin.woelk@web.de>
2021-08-15 21:51:13 +0200

    add Interfaces

--- src/main/java/de/mankianer/todoassistentmono/entities/interfaces/TimeSlot.java
+++ src/main/java/de/mankianer/todoassistentmono/entities/interfaces/TimeSlot.java
@@ -3,4 +3,6 @@ package de.mankianer.todoassistentmono.entities.interfaces;
 public interface TimeSlot {
 
   CalendarEntry getCalenderEntry();
+
+  ToDoFilter getToDoFilter();
 }
---
+++ src/main/java/de/mankianer/todoassistentmono/entities/interfaces/ToDoFilter.java
@@ -0,0 +1,5 @@
+package de.mankianer.todoassistentmono.entities.interfaces;
+
+public interface ToDoFilter {
+
+}
