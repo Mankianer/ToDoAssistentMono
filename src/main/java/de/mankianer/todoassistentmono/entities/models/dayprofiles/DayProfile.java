014d08e4367b8f9fd60fab6ef82e153768af74d9
Mankianer <Marvin.woelk@web.de>
2021-10-25 22:27:01 +0200

    add DayProfileController for MultiClassEntity

--- src/main/java/de/mankianer/todoassistentmono/entities/models/dayprofiles/DayProfile.java
+++ src/main/java/de/mankianer/todoassistentmono/entities/models/dayprofiles/DayProfile.java
@@ -1,7 +1,7 @@
 package de.mankianer.todoassistentmono.entities.models.dayprofiles;
 
 import de.mankianer.todoassistentmono.entities.models.DayContext;
-import de.mankianer.todoassistentmono.entities.models.DgraphEntity;
+import de.mankianer.todoassistentmono.entities.models.DgraphMultiClassEntity;
 import de.mankianer.todoassistentmono.entities.models.dayschemes.DayScheme;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -10,14 +10,14 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 /**
- * Default DayProfile for access DGraphObject.
- * Profile of a DaySchema witch is planed with the Context of a Day.
+ * Default DayProfile for access DGraphObject. Profile of a DaySchema witch is planed with the
+ * Context of a Day.
  */
 @RequiredArgsConstructor
 @NoArgsConstructor
 @Data
-@EqualsAndHashCode(callSuper=false)
-public class DayProfile extends DgraphEntity {
+@EqualsAndHashCode(callSuper = false)
+public class DayProfile extends DgraphMultiClassEntity {
 
   @NonNull
   private String name;
